7a51bb403a05a95982ac1c9df46a44a6e4172455
Kehinde Olawepo <olawepokehinde3@gmail.com>
2026-06-17 01:06:26 -0500

    fix: import arrayRemove - white screen crash on conversation tap

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -14,6 +14,7 @@ import {
   deleteDoc,
   updateDoc,
   arrayUnion,
+  arrayRemove,
 } from "firebase/firestore";
 import { db } from "../firebase";
 import { sendPushNotification } from "../utils/notify";
